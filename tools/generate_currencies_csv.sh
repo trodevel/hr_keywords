a106f0b90fc7b826e8502c7c90b15e431b5bec8d
trodevel <trodevel@gmail.com>
2023-06-21 13:36:14 +0200

    refactor: move currencies to a separate repo (#4)
    * removed currencies
    
    * removed currencies
    
    * removed currencies
    
    * removed currencies

--- tools/generate_currencies_csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# source: https://en.wikipedia.org/wiki/ISO_4217
-
-INP=../raw_resources/currencies.csv
-FL=../resources/currencies.csv
-
-awk -F"\t" '{ printf "%s;%s\n", $2, $1; }' $INP | sed "s/= [0]*/= /"> $FL
